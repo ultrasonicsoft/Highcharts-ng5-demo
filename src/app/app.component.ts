850d470d38e982b079efabedd89b409c3d9860d2
ultrasonicsoft <balramchavan@gmail.com>
2017-12-07 07:36:08 +0530

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { chart } from 'highcharts';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Highcharts + Angular 5 Demo';
+
+  @ViewChild('chartTarget') chartTarget: ElementRef;
+
+  chart: Highcharts.ChartObject;
+  
+  ngAfterViewInit() {
+    const options: Highcharts.Options = {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'John',
+        data: [5, 7, 3]
+      }]
+    };
+  
+    this.chart = chart(this.chartTarget.nativeElement, options);
+  }
+
+  addSeries(){
+    this.chart.addSeries({
+      name:'Balram',
+      data:[2,3,7]
+    })    
+  }
 }
